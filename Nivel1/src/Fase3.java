2dba1656a2bc9ed32978fe71c9b31b8ff00012a0
GuilleRoman <51971998+GuilleRoman@users.noreply.github.com>
2021-06-28 18:10:43 +0200

    Add files via upload

---
+++ Nivel1/src/Fase3.java
@@ -0,0 +1,34 @@
+
+public class Fase3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		final int bisiesto= 1948;
+		int any= 0;
+		int actual= 0;
+		int nacimiento= 1994;
+		boolean cumpleaños=false;
+		String verdadero= "El año 1994 ES un año bisiesto";
+		String falso= "El año 1994 NO es un año bisiesto";
+		System.out.println("Años bisiestos entre 1948 y 1994: ");
+		for (int i= 1948; i<=1994;i++) {
+			if(i==bisiesto || i== actual+4) {
+				actual=i;
+				System.out.print(i+" ");
+				any++;
+				if(actual==nacimiento) {
+					cumpleaños=true;
+				}
+			}
+			
+		}
+		
+		System.out.println("\nNúmero de años bisiestos entre 1948 y 1994: "+ any);
+		if(cumpleaños==true) {
+			System.out.println(verdadero);
+		}else {
+			System.out.println(falso);
+		}
+	}
+
+}
