29552eca60d4926dc76b4d574799b2fdc6ceb812
GuilleRoman <51971998+GuilleRoman@users.noreply.github.com>
2021-06-28 18:11:22 +0200

    Nivel2 del primer proyecto

---
+++ Nivel2/src/main.java
@@ -0,0 +1,13 @@
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double num = 1.4001;
+		int num1 = (int)num;
+		float num2 = (float)num;
+		String num3 = Double.toString(num);
+		System.out.println(num + " Double"+"\n"+num1+" Int"+"\n"+num2+" Float"+"\n"+num3+" String");
+	}
+
+}
