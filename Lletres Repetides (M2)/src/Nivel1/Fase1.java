cdfe85aaae20d9c6c72abf235ea8b3ce7ccba150
GuilleRoman <51971998+GuilleRoman@users.noreply.github.com>
2021-07-08 11:22:14 +0200

    Módulo 2

---
+++ Lletres Repetides (M2)/src/Nivel1/Fase1.java
@@ -0,0 +1,12 @@
+package Nivel1;
+
+public class Fase1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		char[] nom= {'G','u','i','l','l','e','r','m','o'};
+		for(char c:nom) {
+			System.out.print(c);
+		}
+}
+}
