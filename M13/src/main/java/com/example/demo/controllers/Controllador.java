d4317d9174c8ba4361bd178452b99a3a3707a654
Guille <guilleromanm@gmail.com>
2021-10-13 16:14:52 +0200

    M13- Frontend terminado

--- M13/src/main/java/com/example/demo/controllers/Controllador.java
+++ M13/src/main/java/com/example/demo/controllers/Controllador.java
@@ -2,6 +2,7 @@ package com.example.demo.controllers;
 
 import java.util.ArrayList;
 
+import org.springframework.beans.factory.annotation.Autowired;
 //import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -25,9 +26,9 @@ public class Controllador {
 	
 //	DataBaseArrayList db= new DataBaseArrayList();
 
-	DataBaseJDBC db= new DataBaseJDBC();
-	//@Autowired
-	//DataBaseJPAService db;
+	//DataBaseJDBC db= new DataBaseJDBC();
+	@Autowired
+	DataBaseJPAService db;
 	Employee employee;
 	User user;
 	
